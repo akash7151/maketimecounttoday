91c5bc8240bf293fe62a9b263bd136610947d1b7
AkashDake <akashdake9@gmail.com>
2025-05-14 21:29:11 +0530

    Akash

--- src/main/java/com/example/makemytrip/Demo.java
+++ src/main/java/com/example/makemytrip/Demo.java
@@ -1,7 +1,13 @@
 package com.example.makemytrip;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 public class Demo {
     public static void main(String[] args) {
+
+
         System.out.println("Hello");
+        System.out.println("Hello o");
+
     }
 }
