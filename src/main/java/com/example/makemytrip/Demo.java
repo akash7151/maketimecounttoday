20e5d78828fa3d8f66fe86437e6e6da63e1987b9
AkashDake <akashdake9@gmail.com>
2025-05-15 08:17:25 +0530

    up1

--- src/main/java/com/example/makemytrip/Demo.java
+++ src/main/java/com/example/makemytrip/Demo.java
@@ -6,7 +6,7 @@ public class Demo {
     public static void main(String[] args) {
 
 
-        System.out.println("Hello");
+        System.out.println("update");
         System.out.println("Build update");
 
     }
